16ee3b436124e15bb400058bed528295d15fd4f9
Veronika143 <muray.veronika@mail.ru>
2021-10-20 21:17:41 +0500

    Изменения в работе режимов карты

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -21,59 +21,20 @@
                 </form>-->
                 <nav class="header__menu menu">
                 <ul class="menu__list">
-                    <li class="menu__item" id="menu__link__view">
-                    <button type="button" class="menu__link" ><img src="/PageMap/img/header/02.svg" alt="view">Просмотр</button>
+                    <li class="menu__item">
+                    <button type="button" class="menu__link" id="menu__link__view"><img src="/PageMap/img/header/02.svg" alt="view">Просмотр</button>
                     </li>
                     <li class="menu__item" >
                     <button type="button" class="menu__link" id="menu__link__add-object"><img src="/PageMap/img/header/03.svg" alt="object">Добавить объект</button>
                     </li>
-                    <li class="menu__item" id="menu__link__add-route">
-                    <button type="button" class="menu__link" ><img src="/PageMap/img/header/04.svg" alt="route">Добавить маршрут</button>
+                    <li class="menu__item" >
+                    <button type="button" class="menu__link" id="menu__link__add-route"><img src="/PageMap/img/header/04.svg" alt="route">Добавить маршрут</button>
                     </li>
                 </ul>
                 </nav>
             </div>
         </header>
-        <div class="add-object__container hidden">
-            <div class="text-container">
-                <div class="add-object__title">Добавление объекта</div>
-                <div class="add-object__subtitle">Укажите местоположение объекта, передвигая метку на карте.</div>
-            </div>
-            <div class="form-fields">
-                <div class="form-field form-name">
-                    <form action="">
-                        <input type="text" placeholder="Введите название" required>
-                    </form>
-                </div>
-                <div class="form-field form-object-address">
-                    <form action="">
-                        <input type="text" placeholder="Введите адрес" required>
-                    </form>
-                </div>
-                <div class="form-field form-category">
-                    <form action="">
-                        <select name="form-category" required>
-                            <option value="" disabled selected style='display:none;'>Выберите категорию</option>
-                            <option value="Socket"><img src="/PageMap/img/add-object/01.svg" alt="socket">Розетка</option>
-                            <option value="Attraction"><img src="/PageMap/img/add-object/02.svg" alt="socket">Достопримечательность</option>
-                        </select>
-                    </form>
-                </div>
-                <!--<div class="form-description">
-                    <form action="">
-                        <input type="text" name="form-description" placeholder="Добавьте описание">
-                    </form>
-                </div>-->
-                <div class="form-field form-button">
-                    <form action="">
-                        <button type="submit" class="form-photos__add">Добавить</button>
-                    </form>
-                    <!--<form enctype="multipart/form-data" method="post">
-                        <p>Добавьте фотографии весом не более 50 МБ</p>
-                        <input class="add-file" type="file" name="photo" multiple accept="image/*,image/jpeg"></form>-->
-                </div>
-            </div>
-        </div>
+        
         <div class="map" id="mapid"></div>
         <script>
             var mymap = L.map('mapid').setView([56.82, 60.6], 13);
@@ -89,9 +50,10 @@
 
             var popup = L.popup();
 
-            var addObjectForm = document.querySelector('.add-object__container');
             var menuLinks = document.querySelectorAll('.menu__link');
             var lastClicked = menuLinks[0];
+            var viewOnly = false;
+            var addObject = false;
 
             for( var i = 0; i < menuLinks.length; i++ ){
                 menuLinks[i].addEventListener('click', function(){
@@ -102,28 +64,34 @@
             });
             }
 
-            document.addEventListener("click", function(el) {
-                var idItem = el.target.id;
-                if(idItem == 'menu__link__add-object') {
-                    onMapClick(el.target);
-                } else {
-                    addObjectForm.classList.add('hidden');
-                }
+            /*for (var i = 0; i < menuLinks.length; i++) {
+                menuLinks[i].addEventListener('click', function(){
 
-                if(el.target === addObjectForm) {
-                    addObjectForm.classList.add('hidden');
-                }
+                });
+            }*/
+
+            document.getElementById('menu__link__add-object').addEventListener("click", function(e) {
+                addObject = true;
+                viewOnly = false;
+                onMapClick(e.target);
+            });
+
+            document.getElementById('menu__link__view').addEventListener("click", function(e) {
+                addObject = false;
+                viewOnly = true;
+                onMapClick(e.target);
+            });
+
+            document.getElementById('menu__link__add-route').addEventListener("click", function(e) {
+                addObject = false;
+                viewOnly = true;
             });
 
             function onMapClick(e) {
-                // if(addObjectForm.classList.contains('hidden')) {
-                //     addObjectForm.classList.remove('hidden');
-                // }
-
+                if(addObject == true) {
                 popup
                     .setLatLng(e.latlng)
                     .setContent(
-                        // '<div class="add-object__container ">'+
                     '<div class="text-container">'+
                     '<div class="add-object__title">Добавление объекта</div>'+
                 '<div class="add-object__subtitle">Укажите местоположение объекта, передвигая метку на карте.</div>'+
@@ -159,6 +127,7 @@
                     '</div>'+
                     '</div>')
                     .openOn(mymap);
+                }
             }
 
             mymap.on('click', onMapClick);
--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -4,12 +4,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/registration/css/styles.css">
+  <link rel="stylesheet" href="/PageRegistration/css/styles.css">
   <title>Страница регистрации</title>
 </head>
 <body>
   <div class="main-image _ibg">
-    <img src="registration/img/main.png" alt="main">
+    <img src="PageRegistration/img/main.png" alt="main">
   </div>
   <div class="wrapper">
     <div class="logo">LOGO</div>
@@ -44,10 +44,10 @@
       <div class="form-buttons _container">
         <a class="form-button form-button__createacc">Создать аккаунт</a>
         <div class="divider">или</div>
-        <a href="#" class="form-button form-button__google"><img src="registration/img/01.svg" alt="google">Войти с помощью Google</a>
-        <a href="#" class="form-button form-button__vk"><img src="registration/img/02.svg" alt="vk">Войти с помощью ВКонтакте</a>
+        <a href="#" class="form-button form-button__google"><img src="PageRegistration/img/01.svg" alt="google">Войти с помощью Google</a>
+        <a href="#" class="form-button form-button__vk"><img src="PageRegistration/img/02.svg" alt="vk">Войти с помощью ВКонтакте</a>
       </div>
     </div>
-  <!--<script src="/registration/js/script.js"></script>-->
+  <!--<script src="/PageRegistration/js/script.js"></script>-->
 </body>
 </html>
