84af3881152cbde69a2b2c601acdaa1feec4864f
Evgeni2306 <Evgeni2306@outlook.com>
2021-10-20 22:39:11 +0500

    Поправил форму входа

--- resources/views/entrance.blade.php
+++ resources/views/entrance.blade.php
@@ -1,41 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/PageRegistration/css/styles.css">
-  <title>Страница входа</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/PageRegistration/css/styles.css">
+    <title>Страница входа</title>
 </head>
 <body>
-  <div class="main-image _ibg">
+<div class="main-image _ibg">
     <img src="PageRegistration/img/main.png" alt="main">
-  </div>
-  <div class="wrapper">
+</div>
+<div class="wrapper">
     <div class="logo">LOGO</div>
     <div class="text-container _container">
-      <h1 class="text-container__title">Войти в аккаунт</h1>
-      <div class="text-container__subtitle">Еще нет аккаунта? <a href="#" class="text-container__subtitle entrance">Зарегистрироваться<a></div>
+        <h1 class="text-container__title">Войти в аккаунт</h1>
+        <div class="text-container__subtitle">Еще нет аккаунта? <a href="" class="text-container__subtitle entrance">Зарегистрироваться</a></div>
     </div>
     <div class="form-fields _container">
-      <div class="form-field form-field__email">
-        <form action="">
-          <input type="email" placeholder="E-mail" name = 'email'>
-        </form>   
-      </div>
-        <div class="form-field form-field__password">
-          <form action="">
-            <input type="password" placeholder="Пароль">
-          </form>         
-        </div>
-      </div>
-      <div class="form-buttons _container">
+        <form action="" method="">
+            <div class="form-field form-field__email">
+                <input type="email" placeholder="E-mail" name = 'Login'>
+            </div>
+            <div class="form-field form-field__password">
+                <input type="password" placeholder="Пароль" name="Password">
+            </div>
+        </form>
+    </div>
+    <div class="form-buttons _container">
         <a class="form-button form-button__createacc">Войти</a>
         <div class="divider">или</div>
         <a href="#" class="form-button form-button__google"><img src="PageRegistration/img/01.svg" alt="google">Войти с помощью Google</a>
         <a href="#" class="form-button form-button__vk"><img src="PageRegistration/img/02.svg" alt="vk">Войти с помощью ВКонтакте</a>
-      </div>
     </div>
-  <!--<script src="/PageRegistration/js/script.js"></script>-->
+</div>
+<!--<script src="/registration/js/script.js"></script>-->
 </body>
 </html>
