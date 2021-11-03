305a47b0910ff37eb5e212f20d1c2d7942c04086
Evgeni2306 <Evgeni2306@outlook.com>
2021-11-03 12:10:36 +0500

    Поправил форму входа
    Кнопку"Войти" переделал из ссылки в инпут и засунул в форму

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -15,23 +15,29 @@
     <div class="logo">LOGO</div>
     <div class="text-container _container">
         <h1 class="text-container__title">Войти в аккаунт</h1>
-        <div class="text-container__subtitle">Еще нет аккаунта? <a href="{{route('registration')}}" class="text-container__subtitle entrance">Зарегистрироваться</a></div>
+        <div class="text-container__subtitle">Еще нет аккаунта? <a href="{{route('registration')}}"
+                                                                   class="text-container__subtitle entrance">Зарегистрироваться</a>
+        </div>
     </div>
     <form action="" method="">
         <div class="form-fields _container">
             <div class="form-field form-field__email">
-                <input type="email" placeholder="E-mail" name = 'Login'>
+                <input type="email" placeholder="E-mail" name='Login'>
             </div>
             <div class="form-field form-field__password">
                 <input type="password" placeholder="Пароль" name="Password">
             </div>
+        </div>
+        <input type="submit" class="form-button form-button__createacc" value="Войти">
     </form>
-    </div>
+
     <div class="form-buttons _container">
-        <a class="form-button form-button__createacc">Войти</a>
+
         <div class="divider">или</div>
-        <a href="#" class="form-button form-button__google"><img src="PageRegistration/img/01.svg" alt="google">Войти с помощью Google</a>
-        <a href="#" class="form-button form-button__vk"><img src="PageRegistration/img/02.svg" alt="vk">Войти с помощью ВКонтакте</a>
+        <a href="#" class="form-button form-button__google"><img src="PageRegistration/img/01.svg" alt="google">Войти с
+            помощью Google</a>
+        <a href="#" class="form-button form-button__vk"><img src="PageRegistration/img/02.svg" alt="vk">Войти с помощью
+            ВКонтакте</a>
     </div>
 </div>
 <!--<script src="/registration/js/script.js"></script>-->
