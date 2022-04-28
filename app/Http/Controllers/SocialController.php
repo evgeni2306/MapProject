420a41ea1ef8167840416e518be508d457dcc127
Evgeni2306 <Evgeni2306@outlook.com>
2022-04-28 18:57:38 +0500

    1

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -13,11 +13,14 @@ class SocialController extends Controller
 {
     public function googleredirect()
     {
+
         return Socialite::driver('google')->redirect();
     }
 
     public function loginwithgoogle()
     {
+
+
         $user = Socialite::driver('google')->stateless()->user();
         $isUser = User::where('social_id', $user->id)->first();
         if ($isUser) {
@@ -25,18 +28,40 @@ class SocialController extends Controller
             Auth::login($isUser);
             return redirect('map');
         } else {
-            $this->CreateUser($user);
+            $nameAndSurname = explode(' ', $user->name);
+            $createUser = User::create([
+                'name' => $nameAndSurname[0],
+                'surname' => $nameAndSurname[1],
+                'login' => $user->email,
+                'social_id' => $user->id,
+                'password' => encrypt('user'),
+                'avatar' => $user->getAvatar(),
+                'transport' => 'Не указан'
+            ]);
+            $_SESSION['User'] = DB::table('users')
+                ->where('social_id', $user->id)
+                ->select('id', 'name', 'surname', 'avatar', 'transport')
+                ->first();
+
+            Auth::login($createUser);
+            return redirect(route('map'));
+
+
         }
+
 
     }
 
     public function vkontakteredirect()
     {
+
         return Socialite::driver('vkontakte')->redirect();
     }
 
     public function loginwithvkontakte()
     {
+
+
         $user = Socialite::driver('vkontakte')->user();
         $isUser = User::where('social_id', $user->id)->first();
         if ($isUser) {
@@ -44,31 +69,27 @@ class SocialController extends Controller
             Auth::login($isUser);
             return redirect('map');
         } else {
-            $this->CreateUser($user);
+            $nameAndSurname = explode(' ', $user->name);
+            $createUser = User::create([
+                'name' => $nameAndSurname[0],
+                'surname' => $nameAndSurname[1],
+                'login' => $nameAndSurname[0] . time(),
+                'social_id' => $user->id,
+                'password' => encrypt('user'),
+                'avatar' => $user->getAvatar(),
+                'transport' => 'Не указан'
+            ]);
+            $_SESSION['User'] = DB::table('users')
+                ->where('social_id', $user->id)
+                ->select('id', 'name', 'surname', 'avatar', 'transport')
+                ->first();
+
+            Auth::login($createUser);
+            return redirect(route('map'));
+
+
         }
 
 
-    }
-
-    public function CreateUser($user)
-    {
-        $nameAndSurname = explode(' ', $user->name);
-        $createUser = User::create([
-            'name' => $nameAndSurname[0],
-            'surname' => $nameAndSurname[1],
-            'login' => $nameAndSurname[0] . time(),
-            'social_id' => $user->id,
-            'password' => encrypt('user'),
-            'avatar' => $user->getAvatar(),
-            'mapstyle' => 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
-            'transport' => 'Не указан'
-        ]);
-        $_SESSION['User'] = DB::table('users')
-            ->where('social_id', $user->id)
-            ->select('id', 'name', 'surname', 'avatar', 'transport', 'mapstyle')
-            ->first();
-
-        Auth::login($createUser);
-        return redirect(route('map'));
     }
 }
